7bc16dd662045b8546c1f56c1f190af68d9d22e5
Casey Quinn <casey.quinn@bellevuecollege.edu>
2020-02-08 12:13:48 -0800

    Add project files.

---
+++ SettingUpInheritance/Employee.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SettingUpInheritance
+{
+    class Employee
+    {
+        private string firstName;
+        private string lastName;
+        private int empId;
+        private static int numberOfEmployees;
+
+        public string FirstName 
+        {
+            get { return firstName; } 
+            protected set { firstName = value; }///protected is like private for this class and every sub class
+        }
+        public string LastName
+        {
+            get { return lastName; }
+            protected set { lastName = value; }
+        }
+        public int EmpId
+        {
+            get { return empId; }
+            protected set 
+            { if (value < 0)
+                {
+                    //throw new FormatException("Invalid Employee ID");
+                    Console.WriteLine("Invalid Employee ID Detected");
+                    return;
+                }
+                else { empId = value; }
+            }
+        }
+        public static int NumberOfEmployees
+        {
+            get { return numberOfEmployees; }
+            protected set { numberOfEmployees = value; }
+        }
+        public Employee (string fn, string ln, int id)
+        {
+            FirstName = fn;
+            LastName = ln;
+            EmpId = id;
+            NumberOfEmployees++;
+        }
+        public Employee () : this ("unknown","Unknown",0)////always crate a default constructor 
+        {
+
+        }
+        //internal string GetFirstName(string n)
+       // {
+
+       // }
+    }
+}
---
+++ SettingUpInheritance/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace SettingUpInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Employee e0 = new Employee();
+            DisplayEmployeeInfo(e0);
+            //Console.WriteLine("\nEmployee #0: " +
+            //    "\n\tFirst Name: {0}, " +
+            //    "\n\tLast Name: {1}," +
+            //    "\n\tEmployee ID: {2}",
+            //    e0.FirstName, e0.LastName, e0.EmpId);
+          ;
+
+
+            Employee e1 = new Employee("John","Doe", 52);
+            DisplayEmployeeInfo(e1);
+            //Console.WriteLine("\nEmployee #0: " +
+            //"\n\tFirst Name: {0}, " +
+            //"\n\tLast Name: {1}," +
+            //"\n\tEmployee ID: {2}",
+            //e1.FirstName, e1.LastName, e1.EmpId);
+            Console.ReadLine();
+
+            static void DisplayEmployeeInfo(Employee e)
+            {
+                Console.WriteLine("\nEmployee # {3}: " +
+                "\n\tFirst Name: {0}, " +
+                "\n\tLast Name: {1}," +
+                "\n\tEmployee ID: {2}",
+                e.FirstName, e.LastName, e.EmpId,Employee.NumberOfEmployees);
+            }
+
+        }
+    }
+}
